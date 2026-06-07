7e1e570da9ec57e5540c5b0871634b800bb1c926
Jaun <jaunn21@gmail.com>
2026-06-07 15:09:00 +0200

    fix cors

--- API/ChickenFarm.Api/Program.cs
+++ API/ChickenFarm.Api/Program.cs
@@ -31,8 +31,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular", policy =>
         policy.WithOrigins(
             "http://localhost:4200", // Dev
-            "https://chicken-farm-ui.onrender.com" ,// Production
-            "https://chicken-farm-klge.onrender.com
+            "https://chicken-farm-klge.onrender.com"
         )
         .AllowAnyHeader()
         .AllowAnyMethod());
